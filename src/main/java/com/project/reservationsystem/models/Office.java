29530decd4006ee39f4607d71396a9caa9f72e8f
Nikola Jaholcikova <nika.jaholcikova023@gmail.com>
2023-05-18 12:39:10 +0200

    Office entity created

---
+++ src/main/java/com/project/reservationsystem/models/Office.java
@@ -0,0 +1,12 @@
+package com.project.reservationsystem.models;
+
+import java.sql.Time;
+
+public class Office {
+
+  private Long id;
+  private String name;
+  private String description;
+  private Time openingTime;
+  private Time closingTime;
+}
