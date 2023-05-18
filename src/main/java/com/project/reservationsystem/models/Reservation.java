1b4b4a34d06cac97e7e753431acf663826fce30b
Nikola Jaholcikova <nika.jaholcikova023@gmail.com>
2023-05-18 13:45:48 +0200

    Reservation entity created

---
+++ src/main/java/com/project/reservationsystem/models/Reservation.java
@@ -0,0 +1,12 @@
+package com.project.reservationsystem.models;
+
+import java.sql.Time;
+
+public class Reservation {
+
+  private Long id;
+  private Time startingTime;
+  private Time endingTime;
+  //private OfficeUser employee;
+  // private Office office;
+}
